6dd98d9073cf01773711a6304993fc1b7484192d
13020180012 <61999500+13020180012@users.noreply.github.com>
2020-03-10 16:57:08 +0800

    Add files via upload

---
+++ TUGAS 1/Chati.java
@@ -0,0 +1,5 @@
+public class Chati{
+       public static void main(String[] args){
+       System.out.Println("Selamat Belajar");
+}
+}
---
+++ TUGAS 1/pelajar.java
@@ -0,0 +1,5 @@
+public class pelajar{
+       public static void main(String[] args){
+       System.out.ptintln("Selesai Belajar Pemrograman Java");
+}
+}
